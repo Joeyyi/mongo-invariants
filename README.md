4212070aaea558f17721a8a26b0f4338dd8f91fe
Joey <wyjs97115@sina.com>
2019-11-27 15:47:06 -0500

    up and running

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mongo-invariants
 Generate Daikon dynamic invariants for MongoDB
+
+# Usage
+python3 mongo_to_trace.py <path> <host> <port> <database> <level1> <level2>
--- src/mongo_to_trace.py
+++ src/mongo_to_trace.py
@@ -2,6 +2,12 @@ import sys
 import gzip
 import pymongo
 
+def parse_str(mystr):
+  mystr.replace(' ', '_')
+  mystr.replace('\n', '')
+  return mystr
+
+ 
 def write_decls(collections, path):
   with open(path+'.decls', 'w') as out:
     out.write('decl-version 2.0\ninput-language MongoDB\n\n')
@@ -17,6 +23,56 @@ def write_decls(collections, path):
         out.write('    comparability 1' + '\n')
       out.write('\n')
   
+def write(db, path, level_orig, level_new):
+  # collections
+  collections = []
+  colls = db.list_collection_names()
+  for c in colls:
+      temp = { 'name':c }
+      cursor = db[c].find()
+      field_set = set()
+      for document in cursor:
+        for key in document.keys():
+          if key not in field_set:
+            field_set.add(key)
+      temp['fields'] = list(field_set)
+      collections.append(temp)
+
+  # decls
+  with open(path + '.decls', 'w') as decls:
+    decls.write('decl-version 2.0\ninput-language MongoDB\n\n')
+    for coll in collections:
+      decls.write('ppt ' + db.name + '.' + coll['name'] + ':::POINT\n')
+      decls.write('  ppt-type point\n')
+      for field in coll['fields']:
+        decls.write('  variable ' + field + '\n')
+        decls.write('    var-kind variable ' + '\n')
+        decls.write('    rep-type string ' + '\n')
+        decls.write('    dec-type string ' + '\n')
+        decls.write('    comparability 1' + '\n')
+      decls.write('\n')
+
+  # dtrace
+  with open(path + '.dtrace', 'w', encoding="utf-8") as dtrace:
+    dtrace.write('decl-version 2.0\n')
+    for coll in collections:
+      coll_ppt = '\n{0}.{1}:::POINT\n'.format(db.name, coll['name'].replace(' ', '_')) # escaped
+      cursor = db[coll['name']].find()
+      
+      for document in cursor:
+        dtrace.write(coll_ppt)
+        for field in coll['fields']:
+          dtrace.write(field + '\n')
+          if field in document.keys():
+            if type(document[field]).__name__ == 'list':
+              dtrace.write('\"' + str(document[field]).replace('\n','') + '\"\n')
+            else:
+              dtrace.write('\"' + str(document[field]).replace('\n','') + '\"\n')
+          else:
+            dtrace.write('\"' + "default" + '\"\n')            
+          dtrace.write("1")
+          dtrace.write("\n")
+          
 def get_collections(db):
   collections = []
   cols = db.list_collection_names()
@@ -47,11 +103,21 @@ def write_dtrace(db, collections, path):
         
 
 if __name__ == '__main__':
-  path, host, port, database, level_orig, level_new = sys.argv
+  path, host, port, database, level_orig, level_new = sys.argv[1:]
   # client = pymongo.MongoClient("localhost", 27017, maxPoolSize=50)
   client = pymongo.MongoClient(host, int(port), maxPoolSize=50)  
   db = client[database]
-  collections = get_collections(db)
-  write_decls(collections, path)
-  write_dtrace(db, collections, path)
+  # collections = get_collections(db)
+  # write_decls(collections, path)
+  # write_dtrace(db, collections, path)
+  write(db, path, level_orig, level_new)
+
+  # row = db['books'].find_one()
+  # structure = []
+  # for k, v in row.items():
+  #   structure.append(get_structure(v, k, 1, int(level_orig)))
+  # print(structure)
   
+  # st1 = [{'name': '_id', 'type': 'int', 'level': 1}, {'name': 'title', 'type': 'str', 'level': 1}, {'name': 'isbn', 'type': 'str', 'level': 1}, {'name': 'pageCount', 'type': 'int', 'level': 1}, {'name': 'publishedDate', 'type': 'datetime', 'level': 1}, {'name': 'thumbnailUrl', 'type': 'str', 'level': 1}, {'name': 'shortDescription', 'type': 'str', 'level': 1}, {'name': 'longDescription', 'type': 'str', 'level': 1}, {'name': 'status', 'type': 'str', 'level': 1}, {'name': 'authors', 'type': 'list', 'level': 1, 'content': [{'name': 'authors[0]', 'type': 'str', 'level': 2}, {'name': 'authors[1]', 'type': 'str', 'level': 2}, {'name': 'authors[2]', 'type': 'str', 'level': 2}]}, {'name': 'categories', 'type': 'list', 'level': 1, 'content': [{'name': 'categories[0]', 'type': 'str', 'level': 2}, {'name': 'categories[1]', 'type': 'str', 'level': 2}]}]
+  # st2 = [{'name': 'title', 'type': 'str', 'level': 1}, {'name': 'isbn', 'type': 'str', 'level': 1}, {'name': 'pageCount', 'type': 'int', 'level': 1}, {'name': 'publishedDate', 'type': 'datetime', 'level': 1}, {'name': 'thumbnailUrl', 'type': 'str', 'level': 1}, {'name': 'shortDescription', 'type': 'str', 'level': 1}, {'name': 'longDescription', 'type': 'str', 'level': 1}, {'name': 'status', 'type': 'str', 'level': 1}, {'name': 'authors', 'type': 'list', 'level': 1, 'content': [{'name': 'authors[0]', 'type': 'str', 'level': 2}, {'name': 'authors[1]', 'type': 'str', 'level': 2}, {'name': 'authors[2]', 'type': 'str', 'level': 2}]}, {'name': 'categories', 'type': 'list', 'level': 1, 'content': [{'name': 'categories[0]', 'type': 'str', 'level': 2}, {'name': 'categories[1]', 'type': 'str', 'level': 3}]}]
+  # print([item for item in st1 if item not in st2])
